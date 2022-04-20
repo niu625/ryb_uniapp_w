928adbe4d0326b0327892450e1e5441fe8058f88
cookie <1164688561@qq.com>
2022-04-20 13:35:58 +0800

    初始化4.20

---
+++ config/index.js
@@ -0,0 +1,10 @@
+// export const VUE_APP_API_URL = 'http://natapp.xinxintuan.co/api';
+export const VUE_APP_API_URL = 'https://yadmin.api.rybeasyplay.com'   // 测试地址
+// export const VUE_APP_API_URL = '/h5api'
+// export const VUE_APP_API_URL = 'http://139.186.134.205:9006/api'
+// export const VUE_APP_API_URL = 'http://192.168.31.223:8008/api'
+//  export const VUE_APP_API_URL = 'http://natapp.xinxintuan.co/api';
+// export const VUE_APP_API_URL = 'https://thapi.xinxintuan.co/api'
+//  export const VUE_APP_API_URL = 'https://h5api.xinxintuan.co/api';
+// export const VUE_APP_API_URL = 'https://h5api.xinxintuan.co/api';
+export const VUE_APP_RESOURCES_URL = 'https://h5.yixiang.co/static'
